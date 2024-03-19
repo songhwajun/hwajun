9844e9be3ff1137c883421005ea1fee605b9d1b1
SongHwaJun <hwajun.song@danbplus.com>
2024-03-19 17:11:24 +0900

    git 연동 테스트

---
+++ src/main/java/com/danbplus/example/HwajunApplication.java
@@ -0,0 +1,13 @@
+package com.danbplus.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwajunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwajunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danbplus/example/HwajunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danbplus.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwajunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
